010717259ef0c5c1c9e40b023ac12ced745ec28e
Matheus Sousa <ma.sousa.soares@gmail.com>
2022-09-13 23:10:46 -0300

    Configurações adicionais Typescript e Node

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,8 @@ const app = express()
 app.get('/ads', (request, response) => {
   return response.json([
     { id: 1, name: 'Anuncio 1' },
-    { id: 2, name: 'Anuncio 2' }
+    { id: 2, name: 'Anuncio 2' },
+    { id: 3, name: 'Anuncio 3' }
   ])
   //retornar objeto usando chaves,
 })
@@ -17,3 +18,11 @@ app.get('/ads', (request, response) => {
 app.listen(3333)
 
 //node nao entende typescript so javascript, por isso usar apenas no desenvolvimento npm i typescript -D
+//criar um script um build de tsc no json
+//criar arquivo de configuracao do ts npx tsc --init
+//rodar aplicacao tsc npm run build(cria o arquivo js)
+//instalar dependencia do express @types/express
+//ferramenta para sincronizar o node com o ts npm i ts-node-dev -D
+
+//npm run dev - roda o node com restart automatico 
+
