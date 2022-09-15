70c076017d10082d591a0bfa13ca832a47762f09
Matheus Sousa <ma.sousa.soares@gmail.com>
2022-09-15 19:06:44 -0300

    Iniciando landing page

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,25 +1,38 @@
 //JSX = javascript + HTML 
 //npm run dev
-//componentes|Propriedades
-interface BotaoProps{
-  title : string;
-}
+//todas importacoes de css passam por js. o vite adiciona sozinho uma tag link no index.html
+//adicionar tailwind. apos npx tailwind init -pd
+//usar className no react
 
-function Botao(props : BotaoProps){
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import './styles/main.css';
+import logo from './assets/logo.svg';
 
 function App() {
-  return (
-    <div>
-      <Botao title="botao 1" />
-      <Botao title="botao 2" />
-      <Botao title="botao 3" />
-      <Botao title="botao 4" />
+  return ( 
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logo} alt="Logo NLW" />
+      <h1 className='font-black text-6xl text-white mt-20'>
+        Seu <span className='text-transparent bg-duoGradient bg-clip-text'>duo</span> está aqui.</h1>
+        <div className='grid grid-cols-6 gap-6 mt-16'>
+          <a href="">
+            <img src="/image 1.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/image 2.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/image 3.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/image 5.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/image 6.png" alt="" />
+          </a>
+          <a href="">
+            <img src="/image 7.png" alt="" />
+          </a>
+        </div>
     </div>
   )
 }
--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World</Text>
+      <Button titulo='Enviar 1'></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+interface ButtonProps{
+ titulo : string;
+}
+function Button(props : ButtonProps){
+  return (
+  <TouchableOpacity>
+    <Text>
+      {props.titulo}
+    </Text>
+  </TouchableOpacity>
+  );
+}         
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
